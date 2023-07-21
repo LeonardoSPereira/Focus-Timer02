5a479b5692e7fc10340c9deb3e3cc90dd1e75938
Leonardo Pereira <107005097+LeonardoSPereira@users.noreply.github.com>
2023-07-21 15:11:41 -0300

    Update sounds

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,8 @@
 export default function() {
-    const florestAudio = new Audio("../audio/Floresta.wav");
-    const rainAudio = new Audio("../audio/Chuva.wav");
-    const cafeAudio = new Audio("../audio/Cafeteria.wav");
-    const fireAudio = new Audio("../audio/Lareira.wav");
+    const florestAudio = new Audio("https://github.com/LeonardoSPereira/Focus-Timer/blob/main/audio/Floresta.wav?raw=true");
+    const rainAudio = new Audio("https://www.buscasons.com/_arq/2013/Chuva.mp3");
+    const cafeAudio = new Audio("https://github.com/LeonardoSPereira/Focus-Timer/blob/main/audio/Cafeteria.wav?raw=true");
+    const fireAudio = new Audio("https://github.com/LeonardoSPereira/Focus-Timer/blob/main/audio/Lareira.wav?raw=true");
     const timesEnd = new Audio("https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true");
 
     function timeEnd() {
